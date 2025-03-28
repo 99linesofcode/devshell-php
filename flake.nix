e04b53bcf6d56350c9c26c19015afa575f99863d
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-03-28 22:55:56 +0400

    feat: Laravel development environment with PNPM and Xdebug support

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Development environment configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forEachSystem = f: nixpkgs.lib.genAttrs systems f;
+    in
+    {
+      formatter = forEachSystem (s: nixpkgs.legacyPackages.${s}.nixfmt-rfc-style);
+
+      devShells = forEachSystem (
+        s:
+        with nixpkgs.legacyPackages.${s};
+        let
+          phpEnv = php.buildEnv {
+            extensions = (
+              { enabled, all }:
+              enabled
+              ++ (with all; [
+                xdebug
+              ])
+            );
+            extraConfig = ''
+              xdebug.mode=debug
+              xdebug.start_with_request=yes
+            '';
+          };
+        in
+        {
+          default = mkShell {
+            packages = [
+              markdownlint-cli2
+              nodePackages.nodejs
+              nodePackages.pnpm
+              phpEnv
+              phpEnv.packages.composer
+              vale
+              valeStyles.alex
+              valeStyles.google
+              valeStyles.proselint
+            ];
+
+            shellHook = ''
+              export PATH="$PWD/node_modules/.bin/:$PATH"
+            '';
+          };
+        }
+      );
+    };
+}
